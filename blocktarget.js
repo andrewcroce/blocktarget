658f66d0b87b8271a6a632b291dde41d54f0ed7c
Andrew Croce <andrew.croce@gmail.com>
2014-11-13 12:48:06 -0500

    Created an initializer function so it doesn't run automatically, and can be reinitialized. Created an options object that users can pass in to modify default class names.

--- blocktarget.js
+++ blocktarget.js
@@ -1,20 +1,30 @@
 (function($){
 
-$(document).ready(function(){
-	$('.block-target').on('click touchend',function(){
-		var small_target = $(this).find('.block-target-link'),
-			target = small_target.attr('target');
-		if( typeof target === typeof undefined || target === false ){
-			target = '_self';
-		}
-		try{
-			window.open( small_target.attr('href'), target);
-			return false;
-		} catch(e){
-			window.location.href = small_target.attr('href');
-			return false;
-		}
+	$.blocktarget = function( options ){
+
+		var settings = $.extend({
+			'linkClass' : 'block-target-link',
+			'blockClass' : 'block-target'
+		}, options );
+
+		$('.'+settings.blockClass).on('click touchend',function(){
+			var small_target = $(this).find('.'+settings.linkClass),
+				target = small_target.attr('target');
+			if( typeof target === typeof undefined || target === false ){
+				target = '_self';
+			}
+			try{
+				window.open( small_target.attr('href'), target);
+				return false;
+			} catch(e){
+				window.location.href = small_target.attr('href');
+				return false;
+			}
+		});
+	};
+
+	$(document).ready(function(){
+		$.blocktarget();
 	});
-});
 
 })(jQuery);
